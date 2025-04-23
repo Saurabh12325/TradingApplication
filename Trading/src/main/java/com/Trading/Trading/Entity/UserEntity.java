42a7411e597b00301c686d80679a99ed18ed74e6
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-04-24 04:44:55 +0530

    changes

--- Trading/src/main/java/com/Trading/Trading/Entity/UserEntity.java
+++ Trading/src/main/java/com/Trading/Trading/Entity/UserEntity.java
@@ -1,4 +1,24 @@
 package com.Trading.Trading.Entity;
 
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String fullName;
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    private USER_ROLE role;
 }
