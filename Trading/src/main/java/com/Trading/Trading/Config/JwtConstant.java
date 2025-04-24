6698b99f82cde7b379916ad01e5c105037e28616
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-04-24 18:59:38 +0530

    changes

---
+++ Trading/src/main/java/com/Trading/Trading/Config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.Trading.Trading.Config;
+
+public class JwtConstant {
+    public static  final  String SECRET_KEY = "ndkjakkajgajfgkkfljfkjndkfadkagfgjbafkjg";
+    public static final String JWT_HEADER = "Authorization";
+}
