acd57d33bdab91f69faf5b521b73daea18d9fc08
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-04-26 03:58:43 +0530

    changes

--- Trading/src/main/java/com/Trading/Trading/Config/AppConfig.java
+++ Trading/src/main/java/com/Trading/Trading/Config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.Trading.Trading.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,6 +12,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 @Configuration
 public class AppConfig {
 
+   @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.sessionManagement((management->management.sessionCreationPolicy(SessionCreationPolicy.STATELESS)))
         .authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers("/api/**").authenticated()
