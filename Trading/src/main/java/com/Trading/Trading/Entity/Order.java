2cc6a1233d5845702cac25e7b8969de5b35e3a8b
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-05-06 17:23:37 +0530

    creating wallet api

---
+++ Trading/src/main/java/com/Trading/Trading/Entity/Order.java
@@ -0,0 +1,16 @@
+package com.Trading.Trading.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
