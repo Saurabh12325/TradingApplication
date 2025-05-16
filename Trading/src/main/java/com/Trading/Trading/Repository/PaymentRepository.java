945c454bfae502d4f9649b6c0a0594ecd49205c1
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-05-16 15:50:01 +0530

    Payment Integration

--- Trading/src/main/java/com/Trading/Trading/Repository/PaymentRepository.java
+++ Trading/src/main/java/com/Trading/Trading/Repository/PaymentRepository.java
@@ -4,4 +4,5 @@ import com.Trading.Trading.Entity.PaymentOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<PaymentOrder,Long> {
+
 }
--- Trading/src/main/java/com/Trading/Trading/Service/Payment/PaymentServiceImpl.java
+++ Trading/src/main/java/com/Trading/Trading/Service/Payment/PaymentServiceImpl.java
@@ -2,23 +2,44 @@ package com.Trading.Trading.Service.Payment;
 
 import com.Trading.Trading.DTO.PaymentResponse;
 import com.Trading.Trading.Domain.PaymentMethod;
+import com.Trading.Trading.Domain.PaymentOrderStatus;
 import com.Trading.Trading.Entity.PaymentOrder;
 import com.Trading.Trading.Entity.UserEntity;
+import com.Trading.Trading.Repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PaymentServiceImpl implements PaymentService {
+@Autowired
+  private PaymentRepository paymentRepository;
+@Value("${stripe.api.key}")
+  private String stripeSecretKey;
+    @Value("${razorpay.api.key}")
+    private String apiKey;
+    @Value("${razorpay.api.secret}")
+    private String apiSecretKey;
 
     @Override
     public PaymentOrder createOrder(UserEntity user, Long amount, PaymentMethod paymentMethod) {
-        return null;
+        PaymentOrder order = new PaymentOrder();
+        order.setAmount(amount);
+        order.setPaymentMethod(paymentMethod);
+        order.setUser(user);
+
+        return paymentRepository.save(order);
     }
 
     @Override
     public PaymentOrder getPaymentOrderById(Long id) {
-        return null;
+        return paymentRepository.findById(id).orElseThrow(() -> new RuntimeException("Payment order not found"));
     }
 
     @Override
     public Boolean ProceedPaymentOrder(PaymentOrder paymentOrder, String PaymentId) {
+        if(paymentOrder.getStatus().equals(PaymentOrderStatus.PENDING))
+            if(paymentOrder.getPaymentMethod().equals(PaymentMethod.RAZORPAY))
         return null;
     }
 
